e67d260e1bd19cbb0c7e4e1e23c17c5e5efeedd1
wfjsw <wfjsw@users.noreply.github.com>
2019-01-24 09:49:32 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+var B62CHARSET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+const B62decode = (str) => {
+    var res = 0,
+        length = str.length,
+        i, char
+    for (i = 0; i < length; i++) {
+        char = str.charCodeAt(i)
+        if (char < 58) { // 0-9
+            char = char - 48
+        } else if (char < 91) { // A-Z
+            char = char - 29
+        } else { // a-z
+            char = char - 87
+        }
+        res += char * Math.pow(62, length - i - 1)
+    }
+    return res
+};
+
+showdown.extension('stripParagraph', function () {
+    return [{
+        type: 'output',
+        filter: function (txt, conv, opts) {
+            var div = document.createElement('div');
+            var finalTxt = '';
+            div.innerHTML = txt;
+            for (var i = 0; i < div.children.length; ++i) {
+                if (div.children[i].tagName.toLowerCase() === 'p') {
+                    finalTxt += div.children[i].innerHTML;
+                }
+                return finalTxt;
+            }
+        }
+    }]
+})
+
+const md_marker = new showdown.Converter({ extensions: ['stripParagraph'] })
+md_marker.setFlavor('github')
+md_marker.setOption('parseImgDimensions', true)
+md_marker.setOption('headerLevelStart', 3)
+md_marker.setOption('simplifiedAutoLink', true)
+md_marker.setOption('tasklists', true)
+md_marker.setOption('simpleLineBreaks', true)
+md_marker.setOption('strikethrough', true)
+
+
 
 async function fetchJSON(url) {
     const data = await fetch(url)
@@ -23,8 +70,8 @@ function getCRS(tileZoom = 5, offsetX = 0, offsetY = 0) {
 
 function generateMarkerPopupHTML(coord, name, description = "") {
     let html = `<div class="map-marker-popup">\n`
-    html += `<div class="map-marker-title"> <span style="font-weight: bold;">${name}</span></div>\n`
-    if (description) html += `<div class="map-marker-description">${description}</div>`
+    html += `<div class="map-marker-title"> <span style="font-weight: bold;">${name}</span><span class="map-marker-edit-btn mobile-hide"><a href="#" onclick="window.markerEditor.editMarker('${btoa(escape(name))}')"><i class="fa fa-edit"></i></a></span></div>\n`
+    if (description) html += `<div class="map-marker-description">${md_marker.makeHtml(description)}</div>`
     html += `<hr>`
     html += `<div class="map-marker-coords">坐标: ${coord.x}, ${coord.z}</div>`
     html += `</div>\n`
@@ -35,29 +82,36 @@ function generateMarkerListHTML(markers) {
     let html = `<div class="marker-list-container">\n`
     html += `<div class="marker-list-title">地标列表</div>\n`
     html += `<ul class="marker-list-category-list">`
-    for (let c of Object.keys(markers)) {
+    for (let c of Object.keys(markers['markers'])) {
         html += `<li class="marker-list-category">`
         html += `<div class="marker-list-category-title">${c}</div>`
         html += `<ul class="marker-list-marker-list">`
-        for (let m of markers[c]['markers']) {
+        for (let m of markers['markers'][c]) {
             html += `<li class="marker-list-marker">`
             html += `<a class="marker-list-marker-clickable" href="#" onclick="gotoMarkerFromList('${btoa(escape(m.name))}');">${m.name}</a>`
             html += `</li>`
         }
         html += `</ul></li>`
     }
-    html += `</ul></div>\n`
+    html += `</ul>\n`
+    html += `<div class="marker-list-create-btn mobile-hide"><a href="#" onclick="window.markerEditor.addMarker()">+ 添加新地标</a></div>\n`
+    html += `</div>\n`
     return html
 }
 
 async function extractMarkerIcons(marker_data) {
-    const avatar_size = 30
-    async function fetchMarker(url) {
-        const json = await fetchJSON(url)
-        return { url: url, data: L.icon(json) }
+    const avatar_size = 24
+    async function fetchMarker(id, url) {
+        try {
+            const json = await fetchJSON(url)
+            return { url: id, data: L.icon(json) }
+        } catch (e) {
+            console.error(e)
+            return { url: id, data: new L.Icon.Default }
+        }
     }
     async function fetchAvatar(url) {
-        const uuid = url.match(/avatar:\/\/([0-9a-fA-F]{32})/)[1]
+        const uuid = url.match(/^head:([0-9a-fA-F]{32})$/)[1]
         return {
             url, data: L.icon({
                 iconUrl: `https://minotar.net/helm/${uuid}/${avatar_size}`,
@@ -68,18 +122,17 @@ async function extractMarkerIcons(marker_data) {
             })
         }
     }
-    async function markerUrlDispatcher(url) {
-        if (url.match(/https?:\/\//)) return fetchMarker(url)
-        else if (url.match(/avatar:\/\//)) return fetchAvatar(url)
+    async function markerUrlDispatcher(addr) {
+        if (addr.match(/^head:/)) return fetchAvatar(addr)
+        else return fetchMarker(addr, marker_data['icons'][addr])
     }
     let icons = new Map()
-    for (let cat in marker_data) {
-        for (let m of marker_data[cat]['markers']) {
+    for (let cat in marker_data['markers']) {
+        for (let m of marker_data['markers'][cat]) {
             if (m.icon) icons.set(m.icon, m.icon)
         }
-        if (marker_data[cat].icon) icons.set(marker_data[cat].icon, marker_data[cat].icon)
     }
-    let data_raw = await Promise.all([...icons.values()].map(url => markerUrlDispatcher(url)))
+    let data_raw = await Promise.all([...icons.values()].map(addr => markerUrlDispatcher(addr)))
     for (let m of data_raw) {
         icons.set(m.url, m.data)
     }
@@ -93,7 +146,8 @@ async function loadBaseMap(config, map, boundary, meta) {
     for (let m of config.enabled_base_maps) {
         if (!(m in meta['styles'])) continue
         const styleopt = meta['styles'][m]
-        base_maps[styleopt.display_name] = new L.tileLayer(`${config.tiles_server}/z{z}/{style}/{x},{y}.{format}{cache_str}`, {
+        base_maps[styleopt.display_name] = new L.tileLayer(`${config.tiles_server}/{mapid}/z{z}/{style}/{x},{y}.{format}{cache_str}`, {
+            mapid: config.map_id,
             style: m,
             format: config.use_webp_tile && isWebpAvailable ? 'webp' : 'png',
             maxZoom: config.max_zoom,
@@ -110,17 +164,20 @@ async function loadBaseMap(config, map, boundary, meta) {
                 let one_side_tile_num = Math.pow(2, Math.abs(data.z - config.max_tile_zoom))
                 for (let x = data.x * one_side_tile_num; x < (data.x + 1) * one_side_tile_num; x++) {
                     for (let y = data.y * one_side_tile_num; y < (data.y + 1) * one_side_tile_num; y++) {
-                        if (`${x},${y}` in meta['styles'][m]['tiles'] && latest_timestamp < meta['base'] - meta['styles'][m]['tiles'][`${x},${y}`]) {
-                            latest_timestamp = meta['base'] - meta['styles'][m]['tiles'][`${x},${y}`]
+                        if (!(`${x},${y}` in meta['styles'][m]['tiles'])) continue
+                        const cell_timestamp = B62decode(meta['styles'][m]['tiles'][`${x},${y}`])
+                        if (latest_timestamp < cell_timestamp) {
+                            latest_timestamp = cell_timestamp
                         }
                     }
                 }
                 return latest_timestamp > 0 ? `?t=${latest_timestamp}` : ''
             }
         })
-        if (m == config.default_base_map) {
+        if (m === config.default_base_map) {
+            const center = map.getCenter()
             map.options.crs = base_maps[styleopt.display_name].options.crsOverride
-            map._resetView(map.getCenter(), map.getZoom())
+            map._resetView(center, map.getZoom())
             base_maps[styleopt.display_name].addTo(map)
         }
     }
@@ -178,7 +235,7 @@ async function loadMarkers(config, map) {
         let raw_marker_data
         try {
             // Fetch markers
-            raw_marker_data = await fetchJSON(`${config.marker_server}`) // Make sure we have cache-control: no-cache server side.
+            raw_marker_data = window.mapViewer.markers = await window.markerEditor.getMarker(config.marker_server, config.map_id) // await fetchJSON(`${config.marker_server}`) // Make sure we have cache-control: no-cache server side.
             // Marker Data: {what_category: {icon: '', markers: [{x, z, name, description}]}}
         } catch (e) {
             console.error(e)
@@ -193,11 +250,11 @@ async function loadMarkers(config, map) {
         }
         let markers_list = {}
 
-        for (let cat in raw_marker_data) {
+        for (let cat in raw_marker_data['markers']) {
             let markers_group = L.layerGroup()
-            for (let m of raw_marker_data[cat]['markers']) {
+            for (let m of raw_marker_data['markers'][cat]) {
                 markers_list[m.name] = L.marker([m.x + 0.5, m.z + 0.5], {  // Offset applied toward pixel center.
-                    icon: icons_data.get(m.icon) || icons_data.get(raw_marker_data[cat]['icon']) || new L.Icon.Default,
+                    icon: icons_data.get(m.icon) || new L.Icon.Default,
                     title: m.name
                 }).bindPopup(generateMarkerPopupHTML({ x: m.x, z: m.z }, m.name, m.description))
                 markers_list[m.name].addTo(markers_group)
@@ -232,8 +289,8 @@ async function loadMarkers(config, map) {
 
 async function loadPlugin(url, mapViewer) {
     try {
-        const { init } = await import(url)
-        await init(mapViewer)
+        // const { init } = await import(url)
+        // await init(mapViewer)
         return true
     } catch (e) {
         console.error(`Failed to load plugin ${url}`)
@@ -252,7 +309,7 @@ window.initMaps = async function () {
         config = window.mapViewer.config = await fetchJSON('./config.json')  // Make sure we have cache-control: no-cache server side.
         progress.innerText += 'ok\n'
         progress.innerText += '正在加载地图信息... '
-        basemap_meta = await fetchJSON(`${config.tiles_server}/metadata.json`)
+        basemap_meta = await fetchJSON(`${config.tiles_server}/${config.map_id}/metadata.json`)
         progress.innerText += 'ok\n'
     } catch (e) {
         console.error(e)
@@ -273,14 +330,18 @@ window.initMaps = async function () {
         zoomSnap: config.zoom_snap || 0.25,
         zoomDelta: config.zoom_delta || 0.5,
         crs: getCRS(config.max_tile_zoom, 0, 0),
-        maxBounds: view_boundary
+        maxBounds: view_boundary,
+        preferCanvas: true
     })
+
+    window.markerEditor.init(map)
 
     map.on('baselayerchange', (e) => {
         // HACK: https://stackoverflow.com/questions/30862298/in-leaflet-how-to-change-crs-and-other-map-options
         if (e.layer.options.crsOverride) {
+            const center = map.getCenter()
             map.options.crs = e.layer.options.crsOverride
-            map._resetView(map.getCenter(), map.getZoom())
+            map._resetView(center, map.getZoom())
         }
     })
 
@@ -365,14 +426,14 @@ window.initMaps = async function () {
             }
             data.push(item)
         })
-        localStorage['drawn-layers'] = JSON.stringify(data)
+        localStorage.setItem(`drawn-layers:${config.map_id}`, JSON.stringify(data))
         // console.log('draw-tools: saved to localStorage')
     }
 
     const loadDrawnItem = () => {
         try {
-            let dataStr = localStorage['drawn-layers']
-            if (dataStr === undefined) return
+            let dataStr = localStorage.getItem(`drawn-layers:${config.map_id}`)
+            if (dataStr === null) return
 
             let data = JSON.parse(dataStr)
             for (let item of data) {
@@ -459,7 +520,7 @@ window.initMaps = async function () {
     // Load plugins if exist
     if (config.plugins) {
         for (const url of config.plugins) {
-            await loadPlugin(url, window.mapViewer)
+            // await loadPlugin(url, window.mapViewer)
         }
     }
 }
